4442beeadc4edb06d067238281ee7797c807386b
HungBT-Acer <hungbt1908@gmail.com>
2024-01-30 14:52:08 +0700

    add new messenger to change collimator sizes

--- include/DetectorMessenger.hh
+++ include/DetectorMessenger.hh
@@ -10,7 +10,7 @@ class G4UIcmdWithADoubleAndUnit;
 class G4UIcmdWithAString;
 class G4UIcmdWithAnInteger;
 class G4UIcmdWith3VectorAndUnit;
-class  G4UIcmdWithAString;
+class G4UIcmdWithAString;
 class G4UIcmdWithABool;
 
 class DetectorMessenger: public G4UImessenger
@@ -24,27 +24,8 @@ public:
 private:
     DetectorConstruction* fDetectorConstruction;
 
-    G4UIdirectory* GeoDIR;
-    G4UIdirectory* TargetGeoDir;
-    G4UIdirectory* DetectorGeoDir;
-
-    G4UIcmdWithADoubleAndUnit* targetXYDimCmd;
-    G4UIcmdWithADoubleAndUnit* targetZThickCmd;
-    G4UIcmdWithADoubleAndUnit* targetRotationAngleCmd;
-    G4UIcmdWithADoubleAndUnit* baseXYDimCmd;
-    G4UIcmdWithADoubleAndUnit* baseZThickCmd;
-
-    G4UIcmdWithAString* targetMaterialCmd;
-    G4UIcmdWithAString* baseMaterialCmd;
-
-    G4UIcmdWithADoubleAndUnit* det0AngleCmd;
-    G4UIcmdWithADoubleAndUnit* det1AngleCmd;
-    G4UIcmdWithADoubleAndUnit* det2AngleCmd;
-    G4UIcmdWithADoubleAndUnit* det3AngleCmd;
-    G4UIcmdWithADoubleAndUnit* det4AngleCmd;
-    G4UIcmdWithADoubleAndUnit* det5AngleCmd;
-
-    G4UIcmdWithAString* UpdateGeometryCmd;
+    G4UIdirectory* lgkDir;
+    G4UIcmdWithAnInteger* setHelmetSizeCmd;  
 };
 
 
--- src/DetectorMessenger.cc
+++ src/DetectorMessenger.cc
@@ -12,125 +12,27 @@
 DetectorMessenger::DetectorMessenger(DetectorConstruction* pDetectorConstruction)
     : G4UImessenger(),fDetectorConstruction(pDetectorConstruction)
 {
-    GeoDIR = new G4UIdirectory("/HUS/Geometry/");
-    GeoDIR->SetGuidance("............................");
+    //
+    lgkDir = new G4UIdirectory("/LGK/");
+    lgkDir -> SetGuidance("Command to change LGK setups");
 
-    TargetGeoDir = new G4UIdirectory("/HUS/Geometry/Target/");
-    TargetGeoDir->SetGuidance("............................");
-
-    DetectorGeoDir = new G4UIdirectory("/HUS/Geometry/Detector/");
-    DetectorGeoDir->SetGuidance("............................");
-
-
-    targetXYDimCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Target/SetTargetXYDim", this);
-    targetXYDimCmd->SetGuidance("Set Target Size along X and Y axis.");
-    targetXYDimCmd->SetParameterName("TargetXYDim", false);
-    targetXYDimCmd->SetUnitCategory("Length");
-    targetXYDimCmd->SetRange("TargetXYDim>0.0");
-    targetXYDimCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    targetZThickCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Target/SetTargetZThick", this);
-    targetZThickCmd->SetGuidance("Set Target Size along Z axis.");
-    targetZThickCmd->SetParameterName("TargetZThick", false);
-    targetZThickCmd->SetUnitCategory("Length");
-    targetZThickCmd->SetRange("TargetZThick>0.0");
-    targetZThickCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    targetRotationAngleCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Target/SetTargetAngle", this);
-    targetRotationAngleCmd->SetGuidance("Set Target Rotation Angle");
-    targetRotationAngleCmd->SetParameterName("TargetAngle", false);
-    targetRotationAngleCmd->SetUnitCategory("Angle");
-    //targetRotationAngleCmd->SetRange("TargetAngle>0.0");
-    targetRotationAngleCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    baseXYDimCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Target/SetBaseXYDim", this);
-    baseXYDimCmd->SetGuidance("Set Base Size along X and Y axis.");
-    baseXYDimCmd->SetParameterName("BaseXYDim", false);
-    baseXYDimCmd->SetUnitCategory("Length");
-    baseXYDimCmd->SetRange("BaseXYDim>0.0");
-    baseXYDimCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    baseZThickCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Target/SetBaseZThick", this);
-    baseZThickCmd->SetGuidance("Set Base Size along Z axis.");
-    baseZThickCmd->SetParameterName("BaseZThick", false);
-    baseZThickCmd->SetUnitCategory("Length");
-    baseZThickCmd->SetRange("BaseZThick>0.0");
-    baseZThickCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    targetMaterialCmd = new G4UIcmdWithAString("/HUS/Geometry/Target/SetTargetMaterial",this);
-    targetMaterialCmd->SetGuidance("Select Material of the target.");
-    targetMaterialCmd->SetParameterName("TargetMat", false);
-    targetMaterialCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    baseMaterialCmd = new G4UIcmdWithAString("/HUS/Geometry/Target/SetBaseMaterial",this);
-    baseMaterialCmd->SetGuidance("Select Material of the backlayer.");
-    baseMaterialCmd->SetParameterName("BaseMat", false);
-    baseMaterialCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    det0AngleCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Detector/SetDet0Angle", this);
-    det0AngleCmd->SetGuidance("Set DET 0 Rotation Angle");
-    det0AngleCmd->SetParameterName("Det0Angle", false);
-    det0AngleCmd->SetUnitCategory("Angle");
-    //det0AngleCmd->SetRange("Det0Angle>0.0");
-    det0AngleCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    det1AngleCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Detector/SetDet1Angle", this);
-    det1AngleCmd->SetGuidance("Set DET 1 Rotation Angle");
-    det1AngleCmd->SetParameterName("Det1Angle", false);
-    det1AngleCmd->SetUnitCategory("Angle");
-    //det1AngleCmd->SetRange("Det1Angle>0.0");
-    det1AngleCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    det2AngleCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Detector/SetDet2Angle", this);
-    det2AngleCmd->SetGuidance("Set DET 2 Rotation Angle");
-    det2AngleCmd->SetParameterName("Det2Angle", false);
-    det2AngleCmd->SetUnitCategory("Angle");
-    //det2AngleCmd->SetRange("Det2Angle>0.0");
-    det2AngleCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    det3AngleCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Detector/SetDet3Angle", this);
-    det3AngleCmd->SetGuidance("Set DET 3 Rotation Angle");
-    det3AngleCmd->SetParameterName("Det3Angle", false);
-    det3AngleCmd->SetUnitCategory("Angle");
-    //det3AngleCmd->SetRange("Det3Angle>0.0");
-    det3AngleCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    det4AngleCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Detector/SetDet4Angle", this);
-    det4AngleCmd->SetGuidance("Set DET 4 Rotation Angle");
-    det4AngleCmd->SetParameterName("Det4Angle", false);
-    det4AngleCmd->SetUnitCategory("Angle");
-    //det4AngleCmd->SetRange("Det4Angle>0.0");
-    det4AngleCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
-
-    det5AngleCmd = new G4UIcmdWithADoubleAndUnit("/HUS/Geometry/Detector/SetDet5Angle", this);
-    det5AngleCmd->SetGuidance("Set DET 5 Rotation Angle");
-    det5AngleCmd->SetParameterName("Det5Angle", false);
-    det5AngleCmd->SetUnitCategory("Angle");
-    //det5AngleCmd->SetRange("Det5Angle>0.0");
-    det5AngleCmd->AvailableForStates(G4State_PreInit, G4State_Idle);   
+    //
+    setHelmetSizeCmd = new G4UIcmdWithAnInteger("/LGK/SetHelmetSize", this);
+    setHelmetSizeCmd->SetGuidance("Set helmet size(4, 8, 14, 18)");
+    setHelmetSizeCmd->SetParameterName("Size", false);
+    setHelmetSizeCmd->AvailableForStates(G4State_Idle, G4State_PreInit);
 }
 
 DetectorMessenger::~DetectorMessenger()
 {
-    delete GeoDIR;
-    delete DetectorGeoDir;
-    delete targetXYDimCmd;
-    delete targetZThickCmd;
-    delete targetRotationAngleCmd;
-    delete baseXYDimCmd;
-    delete baseZThickCmd;
-    delete targetMaterialCmd;
-    delete baseMaterialCmd;
-    delete det0AngleCmd;
-    delete det1AngleCmd;
-    delete det2AngleCmd;
-    delete det3AngleCmd;
-    delete det4AngleCmd;
-    delete det5AngleCmd;
+    delete lgkDir;
+    delete setHelmetSizeCmd;
 }
 
 void DetectorMessenger::SetNewValue(G4UIcommand* command, G4String newValue)
 {       
-    if(command == targetXYDimCmd)
-    {}
+    if(command == setHelmetSizeCmd)
+    {
+        fDetectorConstruction->SetHelmetSize(setHelmetSizeCmd->GetNewIntValue(newValue));
+    }
 }
