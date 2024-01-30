88a2257464b811af764fa86f61f0120e4079cfe1
HungBT-Acer <hungbt1908@gmail.com>
2024-01-30 11:42:38 +0700

    add v1.0.3

--- MySimulation.cc
+++ MySimulation.cc
@@ -10,6 +10,7 @@
 
 #include "G4Types.hh"
 #include "G4RunManagerFactory.hh"
+#include "G4SteppingVerbose.hh"
 
 #include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
@@ -24,21 +25,78 @@
 
 #include <ctime>
 
+namespace {
+	void PrintUsage() {
+		G4cerr << " Usage: " << G4endl;
+		G4cerr << " ./sim [-m macro ] [-u UIsession] [-t nThreads] [-vDefault]"
+			<< G4endl;
+		G4cerr << "   note: -t option is available only for multi-threaded mode."
+			<< G4endl;
+	}
+}
+
 int main(int argc, char** argv)
 {
+	// Evaluate arguments
+	//
+	if (argc > 7) {
+		PrintUsage();
+		return 1;
+	}
 
-	G4int seconds = time(NULL);
-	G4Random::setTheSeed(seconds);
+	G4String macro;
+	G4String session;
+	G4bool verboseBestUnits = true;
+#ifdef G4MULTITHREADED
+	G4int nThreads = 0;
+#endif
+	for (G4int i = 1; i < argc; i = i + 2) {
+		if (G4String(argv[i]) == "-m") macro = argv[i + 1];
+		else if (G4String(argv[i]) == "-u") session = argv[i + 1];
+#ifdef G4MULTITHREADED
+		else if (G4String(argv[i]) == "-t") {
+			nThreads = G4UIcommand::ConvertToInt(argv[i + 1]);
+		}
+#endif
+		else if (G4String(argv[i]) == "-vDefault") {
+			verboseBestUnits = false;
+			--i;  // this option is not followed with a parameter
+		}
+		else {
+			PrintUsage();
+			return 1;
+		}
+	}
 
-	auto* runManager = G4RunManagerFactory::CreateRunManager();
-	G4int nThreads = 4;
-	runManager->SetNumberOfThreads(nThreads);
+	// Detect interactive mode (if no macro provided) and define UI session
+	//
+	G4UIExecutive* ui = nullptr;
+	if (!macro.size()) {
+		ui = new G4UIExecutive(argc, argv, session);
+	}
 
 	G4ScoringManager::GetScoringManager(); // This enables scoring
 	
 	// Optionally: choose a different Random engine...
 	//
 	G4Random::setTheEngine(new CLHEP::MTwistEngine);
+
+
+	// Use G4SteppingVerboseWithUnits
+	if (verboseBestUnits) {
+		G4int precision = 4;
+		G4SteppingVerbose::UseBestUnit(precision);
+	}
+
+	// Construct the default run manager
+	//
+	auto* runManager =
+		G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
+#ifdef G4MULTITHREADED
+	if (nThreads > 0) {
+		runManager->SetNumberOfThreads(nThreads);
+	}
+#endif
 
 	// Set mandatory initialization classes
 	//
@@ -52,7 +110,7 @@ int main(int argc, char** argv)
 
 	// Initialize G4 kernel
 	//
-	runManager->Initialize();
+	//runManager->Initialize();
 
 	// Initialize visualization
 	//
@@ -62,29 +120,24 @@ int main(int argc, char** argv)
 	// Get the pointer to the User Interface manager
 	auto UImanager = G4UImanager::GetUIpointer();
 
+
 	// Process macro or start UI session
 	//
-	if (argc != 1) {
+	if (macro.size()) {
 		// batch mode
 		G4String command = "/control/execute ";
-		G4String fileName = argv[1];
-		UImanager->ApplyCommand(command + fileName);
+		UImanager->ApplyCommand(command + macro);
 	}
 	else {
 		// interactive mode : define UI session
-
-		G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+		UImanager->ApplyCommand("/control/execute init_vis.mac");
 		UImanager->ApplyCommand("/control/execute defaultMacro.mac");
+		if (ui->IsGUI()) {
+			UImanager->ApplyCommand("/control/execute gui.mac");
+		}
 		ui->SessionStart();
 		delete ui;
-
 	}
-
-	// Job termination
-	// Free the store: user actions, physics_list and detector_description are
-	// owned and deleted by the run manager, so they should not be deleted 
-	// in the main() program !
-
 	delete visManager;
 	delete runManager;
 
